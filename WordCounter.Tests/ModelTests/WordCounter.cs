70c5f3172ada9f1ed036370bd3774d8e5016a395
megan schulte <megan.schulte.pdx@gmail.com>
2019-04-26 08:40:36 -0700

    change folder/file names, add obj folders

---
+++ WordCounter.Tests/ModelTests/WordCounter.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace WordCounterTests
+{
+  [TestClass]
+  public class WordCounterTests
+  {
+
+    // Test methods will go here.
+
+  }
+}
